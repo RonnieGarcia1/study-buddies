2c4fc1db794addd072a388539a7219bb0445c5a2
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-04-28 16:31:30 -0400

    updated seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,5 @@
 \c study_buddies;
 
 INSERT INTO groups (name, main_focus, date_formed, contact_email) VALUES
-('Salomon XT-6', 'Quarry/White/Evening Primrose', 'Great for running ultra-distance races in harsh conditions', 220, 'https://cdn.hiconsumption.com/wp-content/uploads/2021/04/Huckberry-x-Salomon-XT-6-Trail-Shoe-0-Hero.jpg'),
-('Salomon XT-6', 'Wine/Ice Gray', 'Great for running ultra-distance races in harsh conditions', 220, 'https://www.salomon.com/en-us/shop/media/catalog/product/l/4/l41574800_e6wcbmivr0nfc41o.jpg?dpr=2&fit=cover&orient=1&quality=80&optimize=high&format=pjpg&auto=webp&width=2000&height=2000');
+('GroupA', 'Javascript', '12/02/2021 11:00','email@email.com'),
+('GroupB', 'Python', '12/02/2021 11:00','email@email.com');
